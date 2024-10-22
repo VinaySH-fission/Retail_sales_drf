cd6fe19c6275edad5c4d95b2522782e527a51ff6
VinaySH-fission <vinay.hanje@fissionlabs.com>
2024-10-22 11:30:24 +0530

    commiting chages

--- retail_project/retail_project/settings.py
+++ retail_project/retail_project/settings.py
@@ -102,18 +102,6 @@ WSGI_APPLICATION = 'retail_project.wsgi.application'
 
 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'retail_sales',
-#         'USER': 'postgres',
-#         'PASSWORD': '7272',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#     }
-# }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
